39d0d64327ba7d214860f55458231806e76925c2
freewind <nowindlee@gmail.com>
2019-01-01 21:52:27 +0800

    demo

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Override Superagent Types Demo
+=========================================
+
+How to override incorrect superagent types.
+
+The solution here is not perfect since it just creating a new type, and we need to use `as` to force type cast.
+
+But it seems no way to modify existing type directly: https://stackoverflow.com/questions/40322788/how-to-overwrite-incorrect-typescript-type-definition-installed-via-types-packa
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+import * as superagent from 'superagent';
+import {Response} from 'superagent';
+
+interface FixedResponse extends Response {
+  get(header: string): string;
+  get(header: 'Set-Cookie'): string[];
+}
+
+superagent
+    .get('http://test.com')
+    .end((err, response) => {
+      const fixedResponse: FixedResponse = response as FixedResponse;
+      const cookies: string[] = fixedResponse.get('Set-Cookie');
+      const otherHeader: string = fixedResponse.get('others');
+    });
